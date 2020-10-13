767046103a7c112835164ed7aa92c5e694ffb6df
OmorFarukRakib <rakib1001@gmail.com>
2020-10-13 14:40:30 +0600

    Updates

---
+++ precache-manifest.cef61079c733c4a4d11f2c6b62493c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5330499c54e1154537aa9f87bd1e2970",
+    "url": "/Video-Search/index.html"
+  },
+  {
+    "revision": "7cc8472c766d2c384e7e",
+    "url": "/Video-Search/static/css/main.b98e3bfa.chunk.css"
+  },
+  {
+    "revision": "c5fbc400bb344f8928c3",
+    "url": "/Video-Search/static/js/2.b77c6b36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Video-Search/static/js/2.b77c6b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc8472c766d2c384e7e",
+    "url": "/Video-Search/static/js/main.fc4156c4.chunk.js"
+  },
+  {
+    "revision": "07c2956688e218af59d8",
+    "url": "/Video-Search/static/js/runtime-main.94b86584.js"
+  }
+]);
